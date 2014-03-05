8f2b30346ea88f2558b614a3a57cead0c24abff5
Casper Lai <casper.lai@sleepingdesign.com>
2014-03-05 17:48:37 +0800

    Modify BaseController to show the message.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,16 @@
 
 class BaseController extends Controller {
 
+	public function __construct()
+	{
+		$this->beforeFilter('csrf', array('on' => array('post', 'put', 'patch', 'delete')));
+		if(Session::has('message')){
+			$message = is_array(Session::get('message')) ? Session::get('message') : array(Session::get('message'));
+			$message_style = Session::has('style') ? Session::get('style') : 'success';
+			View::share('messages', $message);
+			View::share('message_style', $message_style);
+		}
+	}
 	/**
 	 * Setup the layout used by the controller.
 	 *
