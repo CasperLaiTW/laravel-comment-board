8cda10ada58e5d491a4689672ca63f996ca1730d
Casper Lai <casper.lai@sleepingdesign.com>
2014-03-05 22:56:27 +0800

    Fix button value.

--- app/views/comment/create.blade.php
+++ app/views/comment/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('main')
     {{ Form::model(new \Comment, array('route' => array('comment.store'), 'class' => 'form-horizontal', 'role' => 'form')) }}
-        @include('comment/_partials/_form', ['submit_text' => '儲存'])
+        @include('comment/_partials/_form', ['submit_text' => '新增'])
     {{ Form::close() }}
 @stop
