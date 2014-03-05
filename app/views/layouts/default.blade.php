80ef123f5eb0a49494c1bc209cacf9e6f8147577
Casper Lai <casper.lai@sleepingdesign.com>
2014-03-05 18:33:05 +0800

    Fix partial name incorrect.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -9,7 +9,7 @@
 
     <title>Comment board for bootstrap</title>
     @include('_partials._asset_css')
-    @include('_partials._assset_ie')
+    @include('_partials._asset_ie')
     @yield('asset-css')
   </head>
 
@@ -26,8 +26,6 @@
               <h3 class="masthead-brand">Cover</h3>
               <ul class="nav masthead-nav">
                 <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Features</a></li>
-                <li><a href="#">Contact</a></li>
               </ul>
             </div>
           </div>
@@ -36,18 +34,12 @@
             @yield('main')
           </div>
 
-          <div class="mastfoot">
-            <div class="inner">
-              <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-            </div>
-          </div>
-
         </div>
 
       </div>
 
     </div>
-    @include('_partials._assset_js')
+    @include('_partials._asset_js')
     @yield('asset-js')
   </body>
 </html>
