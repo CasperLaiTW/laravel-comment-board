dba024cff84a7dcdc680bfc2a6f3851ea14d9ec4
Casper Lai <casper.lai@sleepingdesign.com>
2014-03-05 22:45:28 +0800

    Fix css.

--- app/views/_partials/_form_error.blade.php
+++ app/views/_partials/_form_error.blade.php
@@ -1,6 +1,6 @@
 @if($errors->any())
     <div class="space-4"></div>
-    <div class="alert alert-danger">
+    <div class="alert" style="text-align:left;">
         <ul>
             {{ implode('', $errors->all('<li>:message</li>'))}}
         </ul>
